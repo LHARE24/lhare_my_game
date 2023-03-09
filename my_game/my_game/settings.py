5f3dcdfcda33e84d7c4a87649044f397210ec54a
LHARE24 <127256138+LHARE24@users.noreply.github.com>
2023-03-09 12:47:23 -0800

    Added a Mob

---
+++ my_game/my_game/settings.py
@@ -0,0 +1,19 @@
+# File Created by Liam Hare
+
+# Screen Dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# Game Settings
+FPS = 30
+RUNNING = True
+
+# Player Attributes
+PLAYER_ACC  = 2
+PLAYER_FRICTION = -0.12
+MOB_ACC = 2
+MOB_FRICTION = 0.7
+# Colors
+BLACK = (0,0,0)
+BLUE = (50,50,255)
+RED = (255,50,50)
