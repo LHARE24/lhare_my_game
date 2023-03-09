fcc596ba68eec34411804f64b49c0072d6d4c349
LHARE24 <127256138+LHARE24@users.noreply.github.com>
2023-03-09 12:01:52 -0800

    Add files via upload

---
+++ my_game/settings.py
@@ -0,0 +1,17 @@
+# File Created by Liam Hare
+
+# Screen Dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# Game Settings
+FPS = 30
+RUNNING = True
+
+# Player Attributes
+PLAYER_ACC  = 2
+PLAYER_FRICTION = -0.12
+
+# Colors
+BLACK = (0,0,0)
+BLUE = (50,50,255)
